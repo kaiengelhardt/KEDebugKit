c3ab8be020cccf75cfb856a680daf5cf197e1c0f
Kai Engelhardt <kai@linearity.io>
2021-08-08 15:07:53 +0200

    Initial Commit

---
+++ KEDebugKit/KEDebugKit.h
@@ -0,0 +1,18 @@
+//
+//  KEDebugKit.h
+//  KEDebugKit
+//
+//  Created by Kai Engelhardt on 08.08.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KEDebugKit.
+FOUNDATION_EXPORT double KEDebugKitVersionNumber;
+
+//! Project version string for KEDebugKit.
+FOUNDATION_EXPORT const unsigned char KEDebugKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KEDebugKit/PublicHeader.h>
+
+
