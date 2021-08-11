571e19c467b54c6a005f6b1d57ae04624b4f0bd8
Kai Engelhardt <kai@kaiengelhardt.com>
2021-08-11 14:36:46 +0200

    Initial draft of Instruments API

--- Sources/Instruments/Instrument.swift
+++ Sources/Instruments/Instrument.swift
@@ -1,5 +1,5 @@
 //
-//  Created by Kai Engelhardt on 08.08.21.
+//  Created by Kai Engelhardt on 10.08.21
 //  Copyright © 2021 Kai Engelhardt. All rights reserved.
 //
 //  Distributed under the permissive MIT license
@@ -27,28 +27,10 @@
 //
 
 import UIKit
-import KEFoundation
 
-public class DebugWindowController: UIResponder {
+public protocol Instrument: AnyObject {
 
-	private let scene: UIWindowScene
-	private let window: PassthroughWindow
-	private let contentViewController = UIViewController()
-	private var contentView: UIView {
-		contentViewController.view
-	}
+	var title: String { get }
 
-	public init(windowScene: UIWindowScene) {
-		scene = windowScene
-		window = PassthroughWindow(windowScene: windowScene)
-		super.init()
-		setUpUI()
-	}
-
-	private func setUpUI() {
-		window.bounds = scene.screen.bounds
-		window.setFrameToBeNotEntirelyFullscreenToPreventThisWindowFromSwallowingStatusBarEvents()
-		window.rootViewController = contentViewController
-		window.makeKeyAndVisible()
-	}
+	func makeViewController() -> UIViewController
 }
